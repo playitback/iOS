6a32a85ab525a5cc12e913429404835392a6520f
Nick Babenko <nickbabenko@me.com>
2014-06-29 22:04:45 +0100

    Started dropbox sync manager

--- Playback/PBDBSyncManager.swift
+++ Playback/PBDBSyncManager.swift
@@ -8,6 +8,56 @@
 
 import UIKit
 
-class PBDBSync: NSObject {
-   
+class PBDBSyncManager: NSObject {
+		
+	var accountManager:DBAccountManager!
+	var dataStore:DBDatastore?
+	var watchedEntities = String[]()
+	
+	init() {
+		// TODO: Load values from configuration
+		self.accountManager = DBAccountManager(appKey:"", secret:"")
+		
+		if(self.accountManager.linkedAccount) {
+			self.dataStore = DBDatastore.openDefaultStoreForAccount(self.accountManager.linkedAccount, error: nil)
+		}
+	}
+ 
+	func watchEntityWithName(name:String!) {
+		self.watchedEntities.append(name)
+	}
+	
+	func startObserving() {
+		if(!self.dataStore) {
+			return;
+		}
+		
+		self.dataStore?.addObserver(self, block: {
+			if(self.dataStore?.status.incoming) {
+				self.syncDataStore()
+			}
+		})
+	}
+	
+	func stopObserving() {
+		self.dataStore?.removeObserver(self)
+	}
+	
+	func syncDataStore() {
+		var error:DBError
+		var changes:NSDictionary = self.dataStore?.sync(error: error)
+		
+		if(changes) {
+			self.updateCoreDataWithDatastoreChanges(changes)
+		}
+	}
+	
+	func updateCoreDataWithDatastoreChanges(changes:NSDictionary) {
+		changes.enumerateKeysAndObjectsUsingBlock({ (tableId:NSString.self, records:NSArray.self, stop:Boolean.self)
+			for(DBRecord record in records) {
+				
+			}
+		})
+	}
+	
 }
